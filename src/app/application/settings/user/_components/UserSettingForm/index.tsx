0d88a0b9a0cbaf8a85e425e086d5f23be25074bf
HIRO15254 <hi089697@gmail.com>
2024-01-12 01:28:15 +0900

    不要パッケージの削除・Supabaseとの切り離し

--- src/app/application/settings/user/_components/UserSettingForm/index.tsx
+++ src/app/application/settings/user/_components/UserSettingForm/index.tsx
@@ -3,7 +3,6 @@
 import {Group, Tabs} from '@mantine/core';
 import React from 'react';
 
-import {AvatarSettingForm} from '../AvatarSettingForm';
 import {BasicUserSettingForm} from '../BasicUserSettingForm';
 
 /**
@@ -14,13 +13,9 @@ export const UserSettingForm: React.FC = () => (
     <Tabs defaultValue="basic" w="100%" mt="md">
       <Tabs.List>
         <Tabs.Tab value="basic">基本設定</Tabs.Tab>
-        <Tabs.Tab value="icon">アイコン設定</Tabs.Tab>
       </Tabs.List>
       <Tabs.Panel value="basic" pt="xs" px="sm">
         <BasicUserSettingForm />
-      </Tabs.Panel>
-      <Tabs.Panel value="icon" pt="xs" px="sm">
-        <AvatarSettingForm />
       </Tabs.Panel>
     </Tabs>
   </Group>
