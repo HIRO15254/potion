568c5e93e7a407a6ba168095a0245e2ebd4c6a87
HIRO15254 <hi089697@gmail.com>
2024-01-12 01:53:49 +0900

    型エラーを修正
    Storybookはどうにかする

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
 'use client';
 
-import {createTheme} from '@mantine/core';
+import {createTheme, MantineThemeOverride} from '@mantine/core';
 
-export const theme = createTheme({
+export const theme: MantineThemeOverride = createTheme({
   /* Put your mantine theme override here */
 });
