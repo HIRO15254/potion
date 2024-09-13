1bcb2ee13bc7f4a37d02fe152ce25961320a7827
HIRO15254 <hi089697@gmail.com>
2024-09-13 14:14:02 +0900

    名称変更系

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,24 +2,34 @@ import type {MetadataRoute} from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Next.js',
+    name: 'Potion',
+    short_name: 'Potion',
+    description: 'Tools for Japanese poker players',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
+    background_color: "#ffffff",
+    theme_color: "#0070f3",
+    "icons": [
       {
-        src: '/favicons/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        "src": "/favicons/icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
       },
       {
-        src: 'favicons/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        "src": "/favicons/icon-256x256.png",
+        "sizes": "256x256",
+        "type": "image/png"
       },
-    ],
+      {
+        "src": "/favicons/icon-384x384.png",
+        "sizes": "384x384",
+        "type": "image/png"
+      },
+      {
+        "src": "/favicons/icon-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
   }
 }
--- start-database.sh
+++ start-database.sh
@@ -9,7 +9,7 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME="tthree_mantine_template-postgres"
+DB_CONTAINER_NAME="potion-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
@@ -50,6 +50,6 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_USER="postgres" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=tthree_mantine_template \
+  -e POSTGRES_DB=potion \
   -p "$DB_PORT":5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
