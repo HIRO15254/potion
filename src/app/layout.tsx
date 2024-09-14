38a8f21d21e73421f9306590a61b44d9eca6a897
HIRO15254 <hi089697@gmail.com>
2024-09-14 23:42:34 +0900

    fix path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,12 +27,6 @@ export default function RootLayout({
   return (
     <html lang="ja">
     <head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
-      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#0ca678"/>
-      <meta name="msapplication-TileColor" content="#ffffff"/>
-      <meta name="theme-color" content="#ffffff"/>
       <ColorSchemeScript defaultColorScheme="auto"/>
     </head>
     <body>
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,12 +9,12 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     icons: [
       {
-        src: "/android-chrome-192x192.png",
+        src: "favicons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png"
       },
       {
-        src: "/android-chrome-512x512.png",
+        src: "favicons/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png"
       }
